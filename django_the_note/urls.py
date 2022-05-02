31932636e3264978ed620b630e19d0291646f332
KevinDGnanih <kevin.gnanih@gmail.com>
2022-05-02 21:37:22 +0000

    Added the new Database value from heroku after maintenance

--- django_the_note/urls.py
+++ django_the_note/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from the_note_mag.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page, name='home_page')
 ]
--- the_note_mag/views.py
+++ the_note_mag/views.py
@@ -1,5 +1,2 @@
 from django.shortcuts import render
 
-def home_page(request):
-    return render(request, 'p4-the-note/templates/home_page')
-

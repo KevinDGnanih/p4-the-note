d3f12eaea4e4a7367e41f929599bb1955bb17184
KevinDGnanih <kevin.gnanih@gmail.com>
2022-05-02 22:26:58 +0000

    Added the Post model to the models.py file

--- the_note_mag/models.py
+++ the_note_mag/models.py
@@ -1,3 +1,32 @@
+""" Model for The Note Magazine home page """
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Post(models.Model):
+    """ Class for the post """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mag_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='mag_likes', blank=True)
+
+    class Meta:
+        """ Adding methods for a better readability and user experience """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """ The string method to make it easier to read """
+        return self.title
+
+    def number_of_likes(self):
+        """ Counts the number of likes """
+        return self.likes.count()

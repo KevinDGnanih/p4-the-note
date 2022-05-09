e33da5804ffa4f86a0fa8db2464f4aec52e08532
KevinDGnanih <kevin.gnanih@gmail.com>
2022-05-09 22:35:32 +0000

    View comments, open posts and view post list are running

--- the_note_mag/views.py
+++ the_note_mag/views.py
@@ -1,9 +1,11 @@
+""" View file """
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
 
 
 class PostList(generic.ListView):
+    """ Structure the rendering of the post list """
     model = Post
     Post.queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
@@ -11,7 +13,9 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
+    """ Structure the rendering of the post detail """
     def get(self, request, slug, *args, **kwargs):
+        """ Get the information a post """
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')

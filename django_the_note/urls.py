56cac257d5867ca144494af94871b9ed5a11dcda
KevinDGnanih <kevin.gnanih@gmail.com>
2022-05-11 09:00:07 +0000

    Installed allauth library

--- django_the_note/urls.py
+++ django_the_note/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('the_note_mag.urls'), name='the_note_mag_urls'),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
